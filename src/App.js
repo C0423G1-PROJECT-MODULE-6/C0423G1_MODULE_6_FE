41cd21454c333ce337bf290b0902c408dafe2127
buihuuhai318 <131940749+buihuuhai318@users.noreply.github.com>
2023-10-14 22:35:37 +0700

    [HaiBH][user login infomation]

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
 import {ToastContainer} from "react-toastify";
+import LoginForm from "./components/user/LoginForm";
 import {Route, Routes} from "react-router-dom";
+import Information from "./components/user/Information";
+import HomeAdmin from "./components/user/HomeAdmin";
+import { axiosClient } from "./service/user/AxiosClient";
+import Authentication from "./components/user/Authentication";
+import Error403 from "./components/user/Error403";
+import Error401 from "./components/user/Error401";
+import { EnumAppUserRole } from "./components/user/EnumAppUserRole";
 
 function App() {
-  return (
-      <>
-        <ToastContainer></ToastContainer>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
-      </>
-  );
+    axiosClient();
+    return (
+        <>
+            <ToastContainer></ToastContainer>
+            <Routes>
+                <Route path="/401" element={<Error401/>}/>
+                {/*<Route path="*" element={<Home />}></Route>*/}
+                <Route path="/login" element={<LoginForm/>}/>
+                <Route path="/403" element={<Error403/>}/>
+                <Route
+                    element={
+                        <Authentication
+                            allowedRoles={[
+                                EnumAppUserRole.ROLE_ADMIN,
+                                EnumAppUserRole.ROLE_SALE,
+                                EnumAppUserRole.ROLE_BUSINESS,
+                                EnumAppUserRole.ROLE_WAREHOUSE,
+                            ]}
+                        />
+                    }
+                >
+                    <Route path="/admin/information/:id" element={<Information/>}></Route>
+                    <Route path="/admin/home" element={<HomeAdmin/>}></Route>
+                    {/*<Route path="/login" element={<LoginForm />}></Route>*/}
+                </Route>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
