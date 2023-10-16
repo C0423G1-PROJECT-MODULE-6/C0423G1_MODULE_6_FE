c244b8d45bce8a84102d0d7c12fdc90c5253fd4c
hongnguyen0711 <nguyenhongnguyen40@gmail.com>
2023-10-17 01:22:15 +0700

    [NguyenNH][Customer][ShoppingHistoryList]

--- src/service/customer/CustomerService.js
+++ src/service/customer/CustomerService.js
@@ -1,14 +1,33 @@
 import axios from "axios";
 
 const apiURL = "http://localhost:8080/api/admin/customer/list";
+const apiURLHistory = "http://localhost:8080/api/admin/customer/history";
 
 export const getAllPage = async (limit, page, nameSearch, age, gender, sortName, sortCount) => {
-    console.log("check" + nameSearch)
     try {
         const result = await axios.get(apiURL + `?_limit=${limit}&_page=${page}&name_like=${nameSearch}&age=${age}&gender=${gender}&sortName=${sortName}&sortCount=${sortCount}`);
         console.log(result)
         return result;
     } catch (e) {
 
+    }
+}
+
+export const getAllHistory = async (id,limit, page, nameSearch) => {
+    try {
+        const result = await axios.get(apiURLHistory + `/${id}?_limit=${limit}&_page=${page}&name_like=${nameSearch}`);
+        return result;
+    }catch (e) {
+
+    }
+}
+
+export const findById = async (id) => {
+
+    try {
+        const result = await axios.get(apiURL + `/${id}`);
+        return result;
+    } catch (e) {
+        alert("Data not find");
     }
 }
