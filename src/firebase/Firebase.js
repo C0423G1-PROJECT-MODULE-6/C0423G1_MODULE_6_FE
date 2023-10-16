ff657e228da8ec8629e769aa1c216380fa1be466
AnhDao19112001 <phantaanhdao@gmail.com>
2023-10-16 23:57:35 +0700

    [C4ZONE][DaoPTA][config firebase]

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -1,19 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getStorage} from "firebase/storage"
+import {getStorage} from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCfVGXGWlrDYXC5_WKQMBKYX0cd1DfTqtk",
-    authDomain: "uploadingfile-22321.firebaseapp.com",
-    projectId: "uploadingfile-22321",
-    storageBucket: "uploadingfile-22321.appspot.com",
-    messagingSenderId: "351887467989",
-    appId: "1:351887467989:web:8fb3355359d797accec95c"
+    apiKey: "AIzaSyDKS5NsxpVUvRtFM5fZnYGq3J_PPrnqSfc",
+    authDomain: "c4zone-da49c.firebaseapp.com",
+    projectId: "c4zone-da49c",
+    storageBucket: "c4zone-da49c.appspot.com",
+    messagingSenderId: "634858915242",
+    appId: "1:634858915242:web:f31fa94798dd7aab4297d3",
+    measurementId: "G-HG48TKGZKB"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app)
+export const storage = getStorage(app);
