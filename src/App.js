b78d256995133d8f408e2eb79c8326fc543494ea
hongnguyen0711 <nguyenhongnguyen40@gmail.com>
2023-10-17 01:33:09 +0700

    [NguyenNH][Customer][ShoppingHistoryList]

--- src/App.js
+++ src/App.js
@@ -2,29 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 import {ToastContainer} from "react-toastify";
 import {Route, Routes} from "react-router-dom";
+import {CustomerList} from "./components/customer/CustomerList";
+import React from "react";
+import {ShoppingHistoryList} from "./components/customer/ShoppingHistoryList";
 
 function App() {
-  return (
-      <>
-        <ToastContainer></ToastContainer>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
-      </>
-  );
+    return (
+        <>
+            <ToastContainer></ToastContainer>
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                  Learn React
+                </a>
+              </header>
+            </div>
+
+        </>
+    );
 }
 
 export default App;
