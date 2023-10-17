423c1715b904571c0322951a21921fd670a892d1
nguyendinhthoi <dinhthoi2411@gmail.com>
2023-10-17 09:11:59 +0700

    [ThoiND][Order] Handle Cart 17/10/2023

--- src/App.js
+++ src/App.js
@@ -2,27 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 import {ToastContainer} from "react-toastify";
 import {Route, Routes} from "react-router-dom";
+import Order from "./components/order/Order";
 
 function App() {
   return (
       <>
-        <ToastContainer></ToastContainer>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
+         <Order/>
+        {/*<ToastContainer></ToastContainer>*/}
+        {/*<div className="App">*/}
+        {/*  <header className="App-header">*/}
+        {/*    <img src={logo} className="App-logo" alt="logo" />*/}
+        {/*    <p>*/}
+        {/*      Edit <code>src/App.js</code> and save to reload.*/}
+        {/*    </p>*/}
+        {/*    <a*/}
+        {/*        className="App-link"*/}
+        {/*        href="https://reactjs.org"*/}
+        {/*        target="_blank"*/}
+        {/*        rel="noopener noreferrer"*/}
+        {/*    >*/}
+        {/*      Learn React*/}
+        {/*    </a>*/}
+        {/*  </header>*/}
+        {/*</div>*/}
       </>
   );
 }
