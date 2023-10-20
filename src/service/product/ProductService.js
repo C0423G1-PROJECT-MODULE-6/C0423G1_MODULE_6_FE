987c068b0a0882ae02713f27ee9afbaba628be82
damthoaitin1004 <damthoaitin100041997@gmail.com>
2023-10-20 09:06:25 +0700

    [TinDT][Update modal product features for sales and warehouse pages]

--- src/service/product/ProductService.js
+++ src/service/product/ProductService.js
@@ -73,7 +73,7 @@ export const getAllSeries = async () => {
 
 export const getAllType = async () => {
     const result = await axios.get(`http://localhost:8080/api/admin/type/list`)
-    console.log(result)
+    // console.log(result)
     return result.data;
 }
 export const removeProduct = async (id) => {
@@ -89,5 +89,6 @@ export const removeProduct = async (id) => {
  */
 export const getPageProductModal = async (choose,page,value) => {
     const result = await axios.get(`http://localhost:8080/api/admin/product/list?choose=${choose}&page=${page}&value=${value}`)
+    console.log(result)
     return result;
 }
