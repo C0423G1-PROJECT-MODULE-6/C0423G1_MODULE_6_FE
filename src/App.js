82c2751a8c11be2503ec3592a0d36bdb0f93d3ed
AnhDao19112001 <phantaanhdao@gmail.com>
2023-10-23 09:19:31 +0700

    [DaoPTA][fix comment]

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ import ScannerQR from "./components/scanner_qr/ScannerQR";
 import ScannerOrderQR from "./components/scanner_qr/ScannerOrderQR";
 import EditSupplier from "./components/supplier/EditSupplier";
 import { Warehouse } from './components/warehouse/Warehouse';
+import UpdateProduct from "./components/product/UpdateProduct";
 
 
 function App() {
@@ -98,6 +99,9 @@ function App() {
                     <Route path="/admin/business/customer/history/:id" element={<ShoppingHistoryList/>}/>
                     <Route path="/admin/business/order/saleHistory" element={<SaleHistory/>}/>
 
+                    <Route path="/admin/business/product/create" element={<CreateProduct />} />
+                    <Route path="/admin/business/product/list" element={<CreateProduct />} />
+                    <Route path="/admin/business/product/update/:id" element={<UpdateProduct />} />
 
                     /ware
                     <Route path="/admin/ware/warehouse/import/:product" element={<ImportProduct/>}/>
