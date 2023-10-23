28d67f919ae3c1e3a34377cc90c08ebd9322f186
la2305 <lequangphuoc2305@gmail.com>
2023-10-23 11:05:41 +0700

    [phuoc][update employee]

--- src/components/user/EmployeeList.js
+++ src/components/user/EmployeeList.js
@@ -56,6 +56,7 @@ const EmployeeList = () => {
       setEmployeeList([]);
     }
   };
+
   useEffect(() => {
     loadEmployeeList();
   }, [page, searchJob, searchName, searchPhone]);
@@ -107,7 +108,7 @@ const EmployeeList = () => {
       loadEmployeeList();
       setSelectedRow(null);
       setEmployee(null);
-      toast("xóa thành công");
+      toast("Xóa thành công");
     }
   };
 
@@ -173,18 +174,19 @@ const EmployeeList = () => {
             </div>
           </div>
         </div>
-        <div style={{ minHeight: "220px" }}>
+        <div style={{ minHeight: "365px" }}>
           <table className="border border-dark table table-hover">
             <thead>
               <tr>
                 <th style={{ backgroundColor: "darkgray", width: "5%" }}>#</th>
-                <th style={{ backgroundColor: "darkgray", width: "25%" }}>
+                <th style={{ backgroundColor: "darkgray", width: "20%" }}>
                   Họ và tên
                 </th>
+                <th style={{ backgroundColor: "darkgray", width: "10%" }}>Ảnh</th>
                 <th style={{ backgroundColor: "darkgray", width: "10%" }}>
                   Ngày sinh
                 </th>
-                <th style={{ backgroundColor: "darkgray", width: "35%" }}>
+                <th style={{ backgroundColor: "darkgray", width: "30%" }}>
                   Địa chỉ
                 </th>
                 <th style={{ backgroundColor: "darkgray", width: "15%" }}>
@@ -213,7 +215,13 @@ const EmployeeList = () => {
                   >
                     <td>{index + 1}</td>
                     <td>{employee.employeeName}</td>
-                    {/* <td>{employee.employeeBirthday}</td> */}
+                    <td><img src={employee.employeeImage}
+                                                  alt={"không có ảnh"}
+                                                  height="47.5" width="40"
+                                                  style={{
+                                                      borderRadius: "100px",
+                                                      marginRight: "3px"
+                                                  }}/></td>
                     <td>{format(parseISO(employee.employeeBirthday), 'dd-MM-yyyy')}</td>
                     <td>{employee.employeeAddress}</td>
                     <td>{employee.employeeTypeName}</td>
@@ -301,7 +309,6 @@ const EmployeeList = () => {
           </div>
         </div>
       </div>
-      <h1>anhcao</h1>
       <Footer></Footer>
       {/* Modal */}
       <ModalDelete
