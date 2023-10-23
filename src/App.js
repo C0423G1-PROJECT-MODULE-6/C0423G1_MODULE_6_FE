4aaa7cd764fdae0fb31e9372b8eb7366501afca8
NguyenVietCao <vietcao2510@gmail.com>
2023-10-23 14:56:10 +0700

    [CaoNV_Employee][fix app.js]

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ function App() {
                     }
                 >
                     <Route path="/admin/admin/employee" element={<EmployeeList/>}/>
-                    <Route path="/admin/admin/employee/edit" element={<EditEmployee/>}/>
+                    <Route path="/admin/admin/employee/edit/:id" element={<EditEmployee/>}/>
                     <Route path="/admin/admin/employee/create" element={<CreateEmployee/>}/>
                 </Route>
 
